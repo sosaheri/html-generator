35d18359be0367a840923981de34aa6c92878477
rraiomgg <rraiomgg@gmail.com>
2020-08-23 14:49:03 -0700

    adding hello world to index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <?php 
     require_once 'classes/html-generator.php';
     require_once 'static/comandos.php';
-    echo executethis(container,'Piezas4WebSites(hello word)','\(div)');
+    echo executethis(container,'Piezas4WebSites(CUSTOM hello worLd)','\(div)');
 ?>
 </body>
 </html>
