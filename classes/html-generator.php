724f6c3bd75785116da750d3a31be11f5316507e
rraiomgg <rraiomgg@gmail.com>
2020-08-25 03:29:32 -0600

    clase tag y la paleta de colores

--- classes/html-generator.php
+++ classes/html-generator.php
@@ -71,26 +71,6 @@
                 }
             }
         }
-
-        static function executethis($container=null,$content=null,$end=null){
-            if(isset($container)){
-                if($container === 'container(div)/' && $end === '\(div)'){
-                    if(isset($content)){
-                        return tag::get_tag('div',null,null,$content);
-                    }else{
-                        return tag::get_tag('div');
-                    }
-                }else if($container === 'container(header)/' && $end === '\(header)'){
-                    if(isset($content)){
-                        return tag::get_tag('header',null,null,$content);
-                    }else{
-                        return tag::get_tag('header');
-                    }
-                }
-            }else{
-                trigger_error("ingresa por lo menos el primer parametro",E_USER_ERROR);
-            }
-        }
     }
-    
+
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <!DOCTYPE html>
 <html lang="es-MX">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-HTML tag VS _tag</title>
-    <style>
-        /*Paleta de colores para p4wstore*/
-        :root{
-            --azul: #0892A5;
-            --blanco: #FDFFFC;
-            --negro: #00171F;
-            --rojo: #BB0A21;
-            --verde: #23CE6B;
-        }
-    </style>
+    <title>Estructuras HTML</title>
+    <link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap" rel="stylesheet">
 </head>
 <body>
 <?php 
