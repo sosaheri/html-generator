dff485c4fffdb868cd065b551ec786c060f908b6
rraiomgg <rraiomgg@gmail.com>
2020-08-25 05:18:45 -0600

    the awesome function do

--- classes/html-generator.php
+++ classes/html-generator.php
@@ -2,75 +2,78 @@
 
     class tag{
 
-        static function get_tag($comando=null,$id=null,$clase=null,$content=null){
-            if(!isset($comando)){
+        static function do($tag=null,$content=null,$id=null,$clase=null){
+            if(!isset($tag)){
                 trigger_error("ingresa por lo menos el primer parametro",E_USER_ERROR);
             }else{
-                if($comando === 'div'){
-                    if(isset($id) && isset($clase) && isset($content)){
-                        return "<div id=\"{$id}\" class=\"{$clase}\">{$content}</div>";
-                    }else{
-                        if(isset($id) && isset($clase)){
-                            return "<div id=\"{$id}\" class=\"{$clase}\"></div>";
-                        }else{
-                            if(isset($id) && isset($content)){
-                                return "<div id=\"{$id}\">{$content}</div>";
-                            }else{
-                                if(isset($clase) && isset($content)){
-                                    return "<div class=\"{$clase}\">{$content}</div>";
-                                }else{
-                                    if(isset($id)){
-                                        return '<div id="'.$id.'"></div>';
-                                    }else{
-                                        if(isset($clase)){
-                                            return "<div class=\"{$clase}\"></div>";
-                                        }else{
-                                            if(isset($content)){
-                                                return "<div>{$content}</div>";
-                                            }else{
-                                                return '<div></div>';
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                if(isset($id) && isset($clase) && isset($content)){
+                    if(is_array($content)){
+                        $estructura = "<{$tag} id=\"{$id}\" class=\"{$clase}\">";
+                        for($i = 0; $i < sizeof($content); $i++){
+                            $estructura .= $content[$i];
                         }
-                    }
-                }else if($comando === 'header'){
-                    if(isset($id) && isset($clase) && isset($content)){
-                        return "<header id=\"{$id}\" class=\"{$clase}\">{$content}</header>";
+                        $estructura .= "</{$tag}>";
+                        return $estructura;
                     }else{
-                        if(isset($id) && isset($clase)){
-                            return "<header id=\"{$id}\" class=\"{$clase}\"></header>";
-                        }else{
-                            if(isset($id) && isset($content)){
-                                return "<header id=\"{$id}\">{$content}</header>";
-                            }else{
-                                if(isset($clase) && isset($content)){
-                                    return "<header class=\"{$clase}\">{$content}</header>";
-                                }else{
-                                    if(isset($id)){
-                                        return '<header id="'.$id.'"></header>';
-                                    }else{
-                                        if(isset($clase)){
-                                            return "<header class=\"{$clase}\"></header>";
-                                        }else{
-                                            if(isset($content)){
-                                                return "<header>{$content}</header>";
-                                            }else{
-                                                return '<header></header>';
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                        return "<{$tag} id=\"{$id}\" class=\"{$clase}\">{$content}</{$tag}>";
                     }
                 }else{
-                    return 'Comando no definido';
+                    if(isset($id) && isset($clase)){
+                        return "<{$tag} id=\"{$id}\" class=\"{$clase}\"></{$tag}>";
+                    }else{
+                        if(isset($id) && isset($content)){
+                            if(is_array($content)){
+                                $estructura = "<{$tag} id=\"{$id}\">";
+                                for($i = 0; $i < sizeof($content); $i++){
+                                    $estructura .= $content[$i];
+                                }
+                                $estructura .= "</{$tag}>";
+                                return $estructura;
+                            }else{
+                                return "<{$tag} id=\"{$id}\">{$content}</{$tag}>";
+                            }
+                        }else{
+                            if(isset($clase) && isset($content)){
+                                if(is_array($content)){
+                                    $estructura = "<{$tag} class=\"{$clase}\">";
+                                    for($i = 0; $i < sizeof($content); $i++){
+                                        $estructura .= $content[$i];
+                                    }
+                                    $estructura .= "</{$tag}>";
+                                    return $estructura;
+                                }else{
+                                    return "<{$tag} class=\"{$clase}\">{$content}</{$tag}>";
+                                }
+                            }else{
+                                if(isset($id)){
+                                    return '<{$tag} id="'.$id.'"></{$tag}>';
+                                }else{
+                                    if(isset($clase)){
+                                        return "<{$tag} class=\"{$clase}\"></{$tag}>";
+                                    }else{
+                                        if(isset($content)){
+                                            if(is_array($content)){
+                                                $estructura = "<{$tag}>";
+                                                for($i = 0; $i < sizeof($content); $i++){
+                                                    $estructura .= $content[$i];
+                                                }
+                                                $estructura .= "</{$tag}>";
+                                                return $estructura;
+                                            }else{
+                                                return "<{$tag}>{$content}</{$tag}>";
+                                            }
+                                        }else{
+                                            return "<{$tag}></{$tag}>";
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
+
     }
 
 ?>
